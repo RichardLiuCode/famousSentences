782380bd177a689c72794d502a9f607893c526ce
Richard Liu <richardadesigner@gmail.com>
2025-09-14 20:34:52 -0700

    update

--- script.js
+++ script.js
@@ -1,15 +1,19 @@
-import { getHTMLList } from "https://richardliucode.github.io/list/list.js"
+
 document.getElementById("searchBtn").addEventListener("click", function () {
+    document.getElementById("display").innerHTML = ""
     fetch("/data.json")
         .then(function (response) {
             return response.json()
         })
         .then(function (data) {
-            var list = []
+            var container
             for (var i = 0; i < data.length; i = i + 1) {
-                if (data[i].sentence.toLowerCase().includes(document.getElementById("input").value.toLowerCase()))
-                    list.push(data[i].sentence)
+                if (data[i].sentence.toLowerCase().includes(document.getElementById("input").value.toLowerCase())) {
+                    container = document.createElement("div");
+                    container.innerHTML = data[i].sentence.toString()
+                    container.className = "box"
+                    document.getElementById("display").appendChild(container)
+                }
             }
-            document.getElementById("display").innerHTML = getHTMLList(list)
         })
 })
