c0db0d9579f404c5b3c6b3b7738e43a3de67bc63
Richard Liu <richardadesigner@gmail.com>
2025-09-14 20:44:02 -0700

    update

--- script.js
+++ script.js
@@ -1,19 +1,26 @@
 
 document.getElementById("searchBtn").addEventListener("click", function () {
     document.getElementById("display").innerHTML = ""
-    fetch("/data.json")
-        .then(function (response) {
-            return response.json()
-        })
-        .then(function (data) {
-            var container
-            for (var i = 0; i < data.length; i = i + 1) {
-                if (data[i].sentence.toLowerCase().includes(document.getElementById("input").value.toLowerCase())) {
-                    container = document.createElement("div");
-                    container.innerHTML = data[i].sentence.toString()
-                    container.className = "box"
-                    document.getElementById("display").appendChild(container)
+    var checkIsInput = document.getElementById("input").value
+    while (checkIsInput.includes(" ")) {
+        checkIsInput = checkIsInput.replace(" ", "");
+    }
+    if (checkIsInput != "") {
+
+        fetch("/data.json")
+            .then(function (response) {
+                return response.json()
+            })
+            .then(function (data) {
+                var container
+                for (var i = 0; i < data.length; i = i + 1) {
+                    if (data[i].sentence.toLowerCase().includes(document.getElementById("input").value.toLowerCase())) {
+                        container = document.createElement("div");
+                        container.innerHTML = data[i].sentence.toString()
+                        container.className = "box"
+                        document.getElementById("display").appendChild(container)
+                    }
                 }
-            }
-        })
+            })
+    }
 })
